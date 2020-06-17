1ee260179fa4b595dc9a6dbe908b338f254abcf5
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-06-17 20:51:51 -0300

    feat: add structure of tests and exports

---
+++ src/index.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Fireworks from './component/Fireworks';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Fireworks />
+  </React.StrictMode>,
+  document.getElementById('root')
+);
