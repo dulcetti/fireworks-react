f216be75a5e5d34c37cc6316ece4eb7cab6deea4
Bruno Dulcetti <brunodulcetti@gmail.com>
2020-06-17 23:17:27 -0300

    Points animation (#4)
    * feat: add two dots on template component
    
    * style: dots animations
    - refact the arrows animations
    - refact the dots animation to one line
    - change the margins

--- src/component/Fireworks.tsx
+++ src/component/Fireworks.tsx
@@ -3,5 +3,10 @@ import React from 'react';
 import * as S from './Styles';
 
 export default function Fireworks() {
-  return <S.Fireworks />;
+  return (
+    <S.Fireworks>
+      <S.Dots className="top" />
+      <S.Dots className="bottom" />
+    </S.Fireworks>
+  );
 }
